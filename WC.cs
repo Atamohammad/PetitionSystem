0b61cff19621d49ddd71192b7e7295258e55b252
Atamohammad <110150072+Atamohammad@users.noreply.github.com>
2023-06-03 12:26:04 +0430

    Add files via upload

---
+++ WC.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArizaMISNew
+{
+    public class WC
+    {
+
+      
+        public static String ImagePath = @"\Images\Pictures\";
+        public static String AdminRole = "Admin";
+        public static String EntryUserRole = "EntryUser";
+        public static String Viewer = "Viewer";
+    }
+}
